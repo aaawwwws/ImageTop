8c8f360d8ca72ad2ccd0070975e6250ad05e7471
aaawwwws <adeqa1@icloud.com>
2023-09-08 12:13:06 +0900

    プロジェクト ファイルを追加します。

---
+++ image/ImageSelect.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace image
+{
+    internal class ImageSelect
+    {
+        public static OpenFileDialog SelectDialog = new OpenFileDialog();
+        public string FileSelect()
+        {
+            try
+            {
+                SelectDialog.Filter = "イメージファイル(*.png)|*.png|すべてのファイル(*.*)|*.*";
+                SelectDialog.ShowDialog();
+                var FileName = SelectDialog.FileName;
+                MessageBox.Show(FileName);
+                return FileName;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+                return "画像が選択されていません";
+            }
+        } }
+    
+}
---
+++ image/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace image
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static ImageSelect ImageFile = new ImageSelect();
+        private string ImageFileName;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.ImageSet();
+            this.Topmost = true;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        { }
+
+        private void ImageSet()
+        {
+            ImageSelect ImageFIles = new ImageSelect();
+            if (this.ImageFileName == null)
+            {
+                ImageFileName = ImageFIles.FileSelect();
+                BitmapImage ImageUri = new BitmapImage(new Uri(ImageFileName));
+                Image.StretchDirection = StretchDirection.DownOnly;
+                Image.Source = ImageUri;
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+}
